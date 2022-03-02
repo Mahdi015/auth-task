f001dca93b25546f32bb304745a82e9bbf706d9d
Mahdi <ferianimahdi93@gmail.com>
2022-03-02 07:35:53 +0100

    update

--- src/function/admin.js
+++ src/function/admin.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const login = async (values) => {
   return await axios.post(
-    `http://auth-task-jwt.herokuapp.com/api/admin/login`,
+    `https://auth-task-jwt.herokuapp.com/api/admin/login`,
     {
       values,
     }
@@ -11,7 +11,7 @@ export const login = async (values) => {
 
 export const addUser = async (values, authtoken) => {
   return await axios.post(
-    `http://auth-task-jwt.herokuapp.com/api/admin/addUser`,
+    `https://auth-task-jwt.herokuapp.com/api/admin/addUser`,
     { values },
     {
       headers: {
@@ -21,11 +21,11 @@ export const addUser = async (values, authtoken) => {
   );
 };
 export const getUsers = async () =>
-  await axios.get(`http://auth-task-jwt.herokuapp.com/api/admin/getUsers`);
+  await axios.get(`https://auth-task-jwt.herokuapp.com/api/admin/getUsers`);
 
 export const deleteUser = async (userId, authtoken) =>
   await axios.delete(
-    `http://auth-task-jwt.herokuapp.com/api/admin/deleteUser/${userId}`,
+    `https://auth-task-jwt.herokuapp.com/api/admin/deleteUser/${userId}`,
     {
       headers: {
         authtoken,
